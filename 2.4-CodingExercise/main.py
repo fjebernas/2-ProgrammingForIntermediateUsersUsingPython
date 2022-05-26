9eb587c47ddc9543c2f4717adc5d24a4074c9e50
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-05-27 00:24:52 +0800

    Finished and added 2.4 Coding exercise

---
+++ 2.4-CodingExercise/main.py
@@ -0,0 +1,12 @@
+
+flavors = ["vanilla","chocolate","strawberry","cookies n' cream","bubblegum"]
+toppings = ["almonds","banana slices","chocolate syrup","caramel syrup","white chocolate chips"]
+
+ice_cream = dict(zip(flavors,toppings))
+
+print(ice_cream)
+
+ice_cream.update({"chocolate":"blueberries"})
+ice_cream.update({"matcha":"pistachios","ube":"mango slices"})
+
+print(ice_cream)
