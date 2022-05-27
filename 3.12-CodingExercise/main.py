3390cb5be3edb7ee1030480c6b7e178059512cb4
francisjosephironman <francis.3.6.joseph@gmail.com>
2022-05-27 17:41:53 +0800

    Finished and added 3.12 Coding exercise

---
+++ 3.12-CodingExercise/main.py
@@ -0,0 +1,29 @@
+
+from logging import exception
+
+
+# x = 500
+
+# if x > 100:
+#     raise exception("x shouldn't be bigger than 100!")
+
+# try:
+#     print(variable_1)
+# except:
+#     print("variable_1 not declared!")
+
+# for i in range(6):
+#     print("I'm so happy!")
+
+# try:
+#     print(12*6)
+# except:
+#     print("This operation can't be performed")
+# else:
+#     print("This operation can be performed")
+
+best_burger = "Burger King"
+number_2_burger = "McDonald's"
+
+assert best_burger == "Burger King"
+assert best_burger == "McDonald's"
